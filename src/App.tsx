1ee4e1cfdddccb454b166d2ea1d835fdb4577efa
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-02-04 13:50:50 +0900

    typescript finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import Circle from "./Circle";
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div>
-      <Circle bgColor="teal" borderColor="yellow"/>
-      <Circle bgColor="tomato"/>
-    </div>
-  );
+    const [value, setValue] = useState("");
+    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: {value},
+        } = event;
+        setValue(value);
+    };
+    const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("hello", value);
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={value}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="username"/>
+                <button>Log in</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import {DefaultTheme} from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+    bgColor:"white",
+    textColor:"black",
+    btnColor:"tomato",
+};
+
+export const DarkTheme: DefaultTheme ={
+    bgColor:"black",
+    textColor:"white",
+    btnColor:"teal",
+};
