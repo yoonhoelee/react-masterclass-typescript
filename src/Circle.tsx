74cdece0998d8e31a197fd257e83435633ea48e5
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-02-04 13:07:13 +0900

    circle prop

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
+import {useState} from "react";
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
 }
 
 const Container = styled.div<CircleProps>`
@@ -9,10 +11,12 @@ const Container = styled.div<CircleProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border-color: ${(props) => props.borderColor};
 `;
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor }: CircleProps) {
+  const [counter, setCounter] = useState<number|string>(1);
+  return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}></Container>;
 }
 
 export default Circle;
